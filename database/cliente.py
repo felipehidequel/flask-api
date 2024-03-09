b39cd7a8ad6905499f5f12bf4487099507f0a796
Felipe Hidequel <felipehidequel@gmail.com>
2024-03-09 16:10:38 -0300

    added test-database

---
+++ database/cliente.py
@@ -0,0 +1,8 @@
+CLIENTES = [
+    {"id":1, "nome":"Felipe", "email":"teste@teste.com"},
+    {"id":2, "nome":"Lucas", "email":"teste1@teste.com"},
+    {"id":3, "nome":"Pedro", "email":"zaphtrh@teste.com"},
+    {"id":4, "nome":"Jorge", "email":"zap@teste.com"},
+    {"id":5, "nome":"jesus", "email":"zapfdsf@teste.com"},
+    {"id":6, "nome":"NMOSd", "email":"zap431@teste.com"},
+]
